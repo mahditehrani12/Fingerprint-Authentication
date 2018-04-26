7a7cca316c5cd01abe7e0fd4d08b2bb04f6f7558
Mahdi Tehrani <mahditehrani12@gmail.com>
2018-04-26 19:09:21 +0430

    complete UI and build classes

---
+++ app/src/main/java/com/mahdi/fingerprintauthentication/FingerprintHandler.java
@@ -0,0 +1,72 @@
+package com.mahdi.fingerprintauthentication;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.CancellationSignal;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.TextView;
+
+public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
+
+
+    private Context context;
+
+    // Constructor
+    FingerprintHandler(Context mContext) {
+        context = mContext;
+    }
+
+
+    void startAuth(FingerprintManager manager, FingerprintManager.CryptoObject cryptoObject) {
+        CancellationSignal cancellationSignal = new CancellationSignal();
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
+
+
+    @Override
+    public void onAuthenticationError(int errMsgId, CharSequence errString) {
+        this.update("Fingerprint Authentication error\n" + errString, false);
+    }
+
+
+    @Override
+    public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+        this.update("Fingerprint Authentication help\n" + helpString, false);
+    }
+
+
+    @Override
+    public void onAuthenticationFailed() {
+        this.update("Fingerprint Authentication failed.", false);
+    }
+
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        this.update("Fingerprint Authentication succeeded.", true);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() { FingerprintActivity.start(context); }} , 1500 );
+    }
+
+
+    private void update(String e, Boolean success){
+        TextView textView = ((Activity)context).findViewById(R.id.errorText);
+        textView.setText(e);
+        if(success){
+            textView.setTextColor(ContextCompat.getColor(context,R.color.colorPrimaryDark));
+        }
+    }
+}
--- app/src/main/java/com/mahdi/fingerprintauthentication/MainActivity.kt
+++ app/src/main/java/com/mahdi/fingerprintauthentication/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.mahdi.fingerprintauthentication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
